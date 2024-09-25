da4784e6228fa8875a89ccbfbea4da07c60f49c6
Gastón Mousqués <mousques@ort.edu.uy>
2024-09-25 21:08:13 +0000

    tried several models, needs a better retrieval

--- my_ghm_test/rag_llamaindex/rag_bot.py
+++ my_ghm_test/rag_llamaindex/rag_bot.py
@@ -19,7 +19,7 @@ def main():
     current_directory = os.path.dirname(__file__)
     data_path = os.path.join(current_directory, "./data")
     data_base_path = os.path.join(current_directory, "./index_store")
-    bot = RagBasedBot(Mode.RETRIEVE, data_path, data_base_path,"gpt-4o-mini")
+    bot = RagBasedBot(Mode.RETRIEVE, data_path, data_base_path,"gpt-4o")
 
     #while True:
     #    prompt = input("/n/nQué pregunta tienes (o Enter para salir): ")
@@ -30,7 +30,7 @@ def main():
         
         
     ui = gr.ChatInterface(fn=ask, textbox=gr.Textbox(placeholder="Soy <Tag> y puedes preguntar sobre reglamentos y procedimientos de la facultad, asi como también sobre las materias de Diseño de Aplicaciones 1 y Fundamentos de ingeniería", container=True, max_lines=10), title="Tag - el chatbot de la cátedra de IngSoft",
-                          examples=["¿Qué se dicta en diseño de aplicaciones 1?", "¿Qué es un obligatorio?", "¿Como se aprueba un parcial?", "¿Que significa el resultado NSP?"],
+                          examples=["¿Qué se dicta en diseño de aplicaciones 1 (DA1)?", "¿Qué se dicta en Fundamentos de Ingeniería de Software (FIS)?", "¿Qué es un obligatorio?", "¿Como se aprueba un parcial de una materia?", "¿Que significa el resultado NSP?", "Como aviso de una suplencia docente"],
                            clear_btn="Clear",retry_btn=None, undo_btn=None)
     ui.launch(share=True)
     
