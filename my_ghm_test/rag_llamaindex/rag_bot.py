18de97ff5789a69a320be3ca87930e634c31a4da
Gastón Mousqués <mousques@ort.edu.uy>
2024-10-09 21:02:12 +0000

    fixes and cooments

--- my_ghm_test/rag_llamaindex/rag_bot.py
+++ my_ghm_test/rag_llamaindex/rag_bot.py
@@ -38,7 +38,7 @@ def main():
         print("\n Embedding tokens: ", bot.embedding_model.get_token_count())
         
         
-    #ui = gr.ChatInterface(fn=ask, textbox=gr.Textbox(placeholder="Soy <Tag> y puedes preguntar sobre reglamentos y procedimientos de la facultad, asi como también sobre las materias de Diseño de Aplicaciones 1 y Fundamentos de ingeniería", container=True, max_lines=10), title="Tag - el chatbot de la cátedra de IngSoft",
+    #ui = gr.ChatInterface(fn=ask, textbox=gr.Textbox(placeholder="Soy <Tag> y puedes preguntar sobre reglamentos y procedimientos de la facultad y la cátedra de Ingeniería de Software, asi como también sobre las materias de Diseño de Aplicaciones 1 y Fundamentos de ingeniería", container=True, max_lines=10), title="Tag - el chatbot de la cátedra de IngSoft",
     #                     examples=["¿Qué se dicta en diseño de aplicaciones 1 (DA1)?", "¿Qué se dicta en Fundamentos de Ingeniería de Software (FIS)?", "¿Qué es un obligatorio?", "¿Como se aprueba un parcial de una materia?", "¿Que significa el resultado NSP?", "Como aviso de una suplencia docente"],
     #                       clear_btn="Clear",retry_btn=None, undo_btn=None)
     # ui.launch(share=True)
