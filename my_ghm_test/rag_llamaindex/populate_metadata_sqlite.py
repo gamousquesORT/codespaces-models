d2c630e4965fdab3a687fdcecc63ef0bc1faa8c5
Gastón Mousqués <mousques@ort.edu.uy>
2024-10-14 18:59:49 +0000

    improved metadata populate, next embed sources

--- my_ghm_test/rag_llamaindex/populate_metadata_sqlite.py
+++ my_ghm_test/rag_llamaindex/populate_metadata_sqlite.py
@@ -22,7 +22,9 @@ with sqlite3.connect(db_file) as connection:
         internal_source_URL TEXT NOT NULL,
         real_source_URL TEXT NOT NULL,
         retrieval_date TEXT NOT NULL,
-        source_name TEXT NOT NULL
+        source_name TEXT NOT NULL,
+        chunk_size INTEGER DEFAULT 256,
+        chunk_overlaps INTEGER DEFAULT 20
     );
     '''
 
@@ -46,8 +48,13 @@ with sqlite3.connect(db_file) as connection:
 
     data = [
         ('Cátedra', 'Acuerdos', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/Catedra/Acuerdos del curso_DA1.docx', 'https://fi365.sharepoint.com/:w:/s/DiseodeAplicaciones1/Eem2F3XrcPNFlw9fZ1rCq9QBeEl48wesLq23VeJo-FQCjA?e=blDNl5', today, 'Acuerdos del curso_DA1.docx'),
+        ('Cátedra', 'Información', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/Catedra/0_Descripcio Curso Diseño de aplicaciones 1.docx', 'https://fi365.sharepoint.com/:w:/s/DiseodeAplicaciones1/EVE27wjrKY9EnE9lcV-4QzwBe0Q2K3ImpF-7wqTIcdjt6Q?e=g83OJo', today, '0_Descripcio Curso Diseño de aplicaciones 1.docx'),
+        ('Cátedra', 'Información', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/Catedra/1_Temario Teorico Diseño de aplicaciones 1.docx', 'https://fi365.sharepoint.com/:b:/s/DiseodeAplicaciones1/EVuFL6093mBCg0RzSJEvAJ8BtGFPZiJpfFB5FkF6cGiyrQ?e=dG61Pd', today, '1_Temario Teorico Diseño de aplicaciones 1.docx'),
         ('Reglamento', 'Docentes', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/Reglamentos/reglamento-docente__documento-235.pdf', 'https://www.ort.edu.uy/innovaportal/file/95484/1/reglamento-docente__documento-235.pdf', today, 'reglamento-docente__documento-235.pdf'),
-        ('Template', 'Cátedra', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/TemplatesFI/FORMULARIO SUPLENCIA DOCENTE.docx', 'https://fi365.sharepoint.com/:w:/s/IngSoft_Docentes402/EThbs3nuc2BDksP6qoJkw0AB_qDyR4liopLjvTTDQSihHg?e=AAHn0j', today,'FORMULARIO SUPLENCIA DOCENTE.docx')
+        ('Reglamento', 'Facultad', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/Reglamentos/pautas-generales-de-actividad-docente-y-gestion-de-catedras__documento-1041.pdf', 'https://www.ort.edu.uy/innovaportal/file/95484/1/pautas-generales-de-actividad-docente-y-gestion-de-catedras__documento-1041.pdf', today, 'pautas-generales-de-actividad-docente-y-gestion-de-catedras__documento-1041.pdf'),
+        ('Template', 'Facultad', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/TemplatesFI/Planilla para Obligatorio EI 0924.docx', 'https://fi365.sharepoint.com/:w:/s/IngSoft_Docentes402/Ee-AuWx6d8FDlps_hONmI0gB-BtNmSYaHDwoVzHZIZNvuA?e=0gpbeB', today,'Planilla para Obligatorio EI 0924.docx'),
+        ('Template', 'Facultad', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/TemplatesFI/FORMULARIO SUPLENCIA DOCENTE.docx', 'https://fi365.sharepoint.com/:w:/s/IngSoft_Docentes402/EThbs3nuc2BDksP6qoJkw0AB_qDyR4liopLjvTTDQSihHg?e=AAHn0j', today,'FORMULARIO SUPLENCIA DOCENTE.docx'),       
+        ('Template', 'Facultad', '/workspaces/codespaces-models/my_ghm_test/rag_llamaindex/DataSources/TemplatesFI/Plantilla para envío de parciales y exámenes_FI.docx', 'https://fi365.sharepoint.com/:w:/s/IngSoft_Docentes402/EcNp4_bbjytJj4dEGGQVf2gBj9c8I5cUFw-dRFo3p-A8gg?e=iQn0Sc', today,'Plantilla para envío de parciales y exámenes_FI.docx')
     ]
     
     cursor.executemany(insert_query, data)
